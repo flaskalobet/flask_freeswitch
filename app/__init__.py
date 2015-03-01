64e87b31660d3945c73ecd4f6d08516a59688e94
flask.alobet@gmail.com <flask.alobet@gmail.com>
2015-03-01 11:09:58 +0700

    Bootstrap Extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 from config import config
 
+#use bootstrap extension
+from flask.ext.bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    #init bootstrap
+    bootstrap.init_app(app)
 
     # attach routes and custom error pages here
     from main import main as main_blueprint
--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,4 @@ from . import main
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', time=datetime.utcnow())
