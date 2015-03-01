81f50337252c9c2324486ac8c477e4ebfd6eb9ee
flask.alobet@gmail.com <flask.alobet@gmail.com>
2015-03-01 14:54:40 +0700

    flask wtf form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,17 @@
-from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
 
+from .forms import NameForm
+
 @main.route('/')
 def index():
-    return render_template('index.html', time=datetime.utcnow())
+    return render_template('index.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = NameForm()
+    name = None
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('login.html', form=form, name=name)
